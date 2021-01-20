525424d7a858ba7f1abfd83f873e308badc73e21
Muye Chen <echennj@gmail.com>
2021-01-19 22:57:08 -0500

    Initial commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+import sys
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+r = requests.get('https://coreyms.com')
+print(r.status_code)
