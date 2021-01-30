02cbbe669fca07c305019407a54ad244621231fe
Muye Chen <muye_chen@apple.com>
2021-01-30 12:04:14 -0500

    remove code

--- hello.py
+++ hello.py
@@ -1,20 +1,5 @@
-import math
-import sys
-from os import rename
 import requests
 
-print(sys.version)
-print(sys.executable)
 
-
-def greet(who_to_greet):
-    test = "Test"
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-# r = requests.get("https://coreyms.com")
-# print(r.status_code)
-
-name = input("Your name?")
-print("Hello", name)
+r = requests.get("https://coreyms.com")
+print(r.status_code)
